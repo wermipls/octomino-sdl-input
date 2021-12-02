94cbfe1de7e21a6c9d05395f301504796cb01a7f
wermi <32251376+wermipls@users.noreply.github.com>
2021-12-03 00:03:19 +0100

    add license info

--- src/gui.c
+++ src/gui.c
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include "gui_renderer.h"
